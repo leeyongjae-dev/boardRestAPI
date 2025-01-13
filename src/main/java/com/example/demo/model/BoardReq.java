5a2df84b0a366755e173f9c89eafef99ef800e4a
BOOK-BKT8UBVE0A\dhfkd <dhfkdl26@naver.com>
2025-01-13 11:09:14 +0900

    수정 커밋

--- src/main/java/com/example/demo/model/BoardReq.java
+++ src/main/java/com/example/demo/model/BoardReq.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
--- src/main/java/com/example/demo/model/CommonBoardReq.java
+++ src/main/java/com/example/demo/model/CommonBoardReq.java
@@ -2,21 +2,26 @@ package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CommonBoardReq {
 
 	@JsonProperty("currPage")
+	@Schema(example = "1")
 	private int currPage;
 
 	@JsonProperty("rowCount")
+	@Schema(example = "10")
 	private int rowCount;
 
 	@JsonProperty("startRow")
+	@Schema(example = "1")
 	private Integer startRow;
 
 	@JsonProperty("endRow")
+	@Schema(example = "10")
 	private int endRow;
 
 }
